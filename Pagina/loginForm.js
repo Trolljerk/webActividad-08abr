e4ffcfab089a5ae8a90966fa7cc67fee37bfe2a4
Samir Alexander Conde Pabón <90851389+Trolljerk@users.noreply.github.com>
2024-04-08 11:56:51 -0500

    Add files via upload

---
+++ Pagina/loginForm.js
@@ -0,0 +1,21 @@
+const form = document.forms['loginForm'];
+
+form.addEventListener('submit', function handleFormSubmit(event) {
+    event.preventDefault();
+
+    const email = form['email'].value;
+    const password = form['password'].value;
+
+    loginUser(email, password);
+});
+
+function loginUser(email, password) {
+    firebase.auth().signInWithEmailAndPassword(email, password)
+        .then(function (user) {
+            console.log('Credenciales correctas, ¡bienvenido! ' + user);
+            localStorage.setItem('user', JSON.stringify(user));
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+}
